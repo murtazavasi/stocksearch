4a799df1fc478821dae94a3cc4bfaebbd8ee2383
Murtaza Vasi <murtazavasi2000@gmail.com>
2024-03-28 22:12:36 +0530

    Recommendations chart fixed

--- stocksearch-frontend/src/components/RecommendationTrendsChart.js
+++ stocksearch-frontend/src/components/RecommendationTrendsChart.js
@@ -9,35 +9,101 @@ const RecommendationTrendsChart = ({ ticker }) => {
 	const [recommendationTrends, setRecommendationTrends] = useState([]);
 	const [categories, setCategories] = useState([]);
 	const [loading, setLoading] = useState(false);
+	const [chartOptions, setChartOptions] = useState(null);
 
 	const fetchColumnChartData = async () => {
 		const response = await axios.get(`/stock/recommendation-trends/${ticker}`);
 		const data = await response.data;
 
-		let buy = [];
-		let sell = [];
-		let hold = [];
-		let strongBuy = [];
-		let strongSell = [];
+		// Extract all keys present in the input data
+		const allKeys = Array.from(
+			new Set(data.flatMap((item) => Object.keys(item)))
+		);
 
-		for (let item in data) {
-			buy.push(item["buy"]);
-			sell.push(item["sell"]);
-			hold.push(item["hold"]);
-			strongBuy.push(item["strongBuy"]);
-			strongSell.push(item["strongSell"]);
-		}
+		// Initialize an object to store values for each key
+		const valuesByKeys = {};
 
-		const categoriesResult = data.map((item) => [item["period"]]);
-		const recommendationTrendsResult = [
-			{ data: buy },
-			{ data: sell },
-			{ data: hold },
-			{ data: strongBuy },
-			{ data: strongSell },
-		];
-		setRecommendationTrends(recommendationTrendsResult);
-		setCategories(categoriesResult);
+		// Iterate over each key
+		allKeys.forEach((key) => {
+			// Initialize an array for the values corresponding to the key
+			valuesByKeys[key] = [];
+
+			// Push the values corresponding to the key from each object into the array
+			data.forEach((item) => {
+				valuesByKeys[key].push(item[key]);
+			});
+		});
+
+		console.log(valuesByKeys);
+
+		let finalData = [];
+		let categories = [];
+		Object.entries(valuesByKeys).forEach(([key, value]) => {
+			if (key !== "period" && key !== "symbol") {
+				finalData.push({
+					name: key,
+					data: Array.from(value),
+				});
+			}
+			if (key === "period") {
+				categories = Array.from(value);
+			}
+		});
+		console.log(finalData);
+		updateOptions(categories, finalData);
+		setRecommendationTrends(data);
+		setCategories(categories);
+	};
+
+	const updateOptions = (categories, finalData) => {
+		const options = {
+			chart: {
+				type: "column",
+			},
+			title: {
+				text: "Recommendations Trends",
+			},
+			xAxis: {
+				categories: categories,
+			},
+			yAxis: {
+				min: 0,
+				title: {
+					text: "#Analysis",
+				},
+				stackLabels: {
+					enabled: true,
+				},
+				opposite: false,
+			},
+			tooltip: {
+				headerFormat: "<b>{point.x}</b><br/>",
+				pointFormat: "{series.name}: {point.y}<br/>Total: {point.stackTotal}",
+			},
+			plotOptions: {
+				column: {
+					stacking: "normal",
+					dataLabels: {
+						enabled: true,
+					},
+				},
+			},
+			series: finalData,
+			rangeSelector: {
+				enabled: false,
+			},
+			navigator: {
+				enabled: false,
+			},
+			scrollbar: {
+				enabled: false,
+			},
+			legend: {
+				enabled: true,
+			},
+		};
+		console.log(options);
+		setChartOptions(options);
 	};
 
 	useEffect(() => {
@@ -50,28 +116,22 @@ const RecommendationTrendsChart = ({ ticker }) => {
 		return <h1>Loading</h1>;
 	}
 
-	const options = {
-		xAxis: {
-			categories: categories,
-		},
-		plotOptions: {
-			column: {
-				stacking: "normal",
-				dataLabels: {
-					enabled: true,
-				},
-			},
-		},
-		series: recommendationTrends,
-	};
 	return (
-		<HighchartsReact
-			highcharts={HighCharts}
-			constructorType={"stockChart"}
-			options={options}
-		>
-			RecommendationTrendsChart
-		</HighchartsReact>
+		<h1>
+			{loading ? (
+				<h1>Loading</h1>
+			) : chartOptions ? (
+				<HighchartsReact
+					highcharts={HighCharts}
+					constructorType={"stockChart"}
+					options={chartOptions}
+				>
+					RecommendationTrendsChart
+				</HighchartsReact>
+			) : (
+				<div>Failed to fetch data</div>
+			)}
+		</h1>
 	);
 };
 
